829718627f774fc67b4dd1ac8b3f5a91c73fe2e8
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2025-03-15 19:18:03 +0800

    fix snac build

--- web/snac/BUILD.mk
+++ web/snac/BUILD.mk
@@ -5,11 +5,14 @@ SNAC_DEPS = lib/openssl web/curl lib/certs
 $(eval $(call DOWNLOAD_SOURCE,web/snac,$(SNAC_SRC)))
 $(eval $(call SPECIFY_DEPS,web/snac,$(SNAC_DEPS)))
 
-o/web/snac/build.x86_64: BUILD_COMMAND = $(DUMMYLINK0)
-o/web/snac/build.aarch64: BUILD_COMMAND = $(DUMMYLINK0)
+o/web/snac/configured.x86_64: CONFIG_COMMAND = $(DUMMYLINK0)
+o/web/snac/configured.aarch64: CONFIG_COMMAND = $(DUMMYLINK0)
 
-o/web/snac/installed.x86_64: INSTALL_COMMAND = $(DUMMYLINK0)
-o/web/snac/installed.aarch64: INSTALL_COMMAND = $(DUMMYLINK0)
+# o/web/snac/built.x86_64: BUILD_COMMAND = $(BUILD_DEFAULT)
+# o/web/snac/built.aarch64: BUILD_COMMAND = $(BUILD_DEFAULT)
+
+# o/web/snac/installed.x86_64: INSTALL_COMMAND = $(INSTALL_DEFAULT)
+# o/web/snac/installed.aarch64: INSTALL_COMMAND = $(INSTALL_DEFAULT)
 
 o/web/snac/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
 o/web/snac/built.fat: BINS = snac
