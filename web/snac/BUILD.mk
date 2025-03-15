35c32f500fd8ad575091c228c148263da83d63d3
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2025-03-15 11:17:47 +0800

    fix snac build

--- web/snac/BUILD.mk
+++ web/snac/BUILD.mk
@@ -1,15 +1,12 @@
 
 SNAC_SRC = https://codeberg.org/grunfink/snac2/archive/2.73.tar.gz
-SNAC_DEPS = lib/openssl \
-			web/curl \
-			lib/certs \
-			cosmo-repo/base cosmo-repo/compress
+SNAC_DEPS = lib/openssl web/curl lib/certs
 
 $(eval $(call DOWNLOAD_SOURCE,web/snac,$(SNAC_SRC)))
 $(eval $(call SPECIFY_DEPS,web/snac,$(SNAC_DEPS)))
 
-o/web/snac/builded.x86_64: BUILD_COMMAND = $(DUMMYLINK0)
-o/web/snac/builded.aarch64: BUILD_COMMAND = $(DUMMYLINK0)
+o/web/snac/build.x86_64: BUILD_COMMAND = $(DUMMYLINK0)
+o/web/snac/build.aarch64: BUILD_COMMAND = $(DUMMYLINK0)
 
 o/web/snac/installed.x86_64: INSTALL_COMMAND = $(DUMMYLINK0)
 o/web/snac/installed.aarch64: INSTALL_COMMAND = $(DUMMYLINK0)
